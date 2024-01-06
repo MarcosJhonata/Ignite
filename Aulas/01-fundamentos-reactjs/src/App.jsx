e264b3ea3d21846ac24337b0adcbd5862f8f5803
Marcos Jhonata <marcosjhonata23@gmail.com>
2024-01-06 19:41:24 -0300

    Add component Header and Header css module , and style css for background

--- Aulas/01-fundamentos-reactjs/src/App.jsx
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,10 @@
-import { Post } from './Post'
+import { Post } from './Post';
+import { Header } from './components/Header';
+import './style.css';
 export function App() {
   return (
      <div>
+      <Header />
      <Post 
      author="Marcos jhonata"
      content="Treinamento react"
