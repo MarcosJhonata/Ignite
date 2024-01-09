4f1f346ad3f0a7158116430459c7e582be9e507c
Marcos Jhonata <marcosjhonata23@gmail.com>
2024-01-08 22:16:11 -0300

    Add sidebar with styles and components , add icon on editar perfil

--- Aulas/01-fundamentos-reactjs/src/App.jsx
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,26 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+
 export function App() {
   return (
      <div>
       <Header />
-     <Post 
-     author="Marcos jhonata"
-     content="Treinamento react"
+     <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+      <Post 
+      author="Marcos jhonata"
+      content="Treinamento react"
      />
+     <Post 
+      author="Thais Amanda"
+      content="Treinamento react"
+     />
+      </main>
+     </div>
      </div>
   
   )
